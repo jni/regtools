bc4d651b89b752241ac6e28ddc83361afd6b2b9f
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-10-01 16:49:44 +1000

    Add tests for metadata reading

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,29 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os
 
-"""
-test_regtools
-----------------------------------
-
-Tests for `regtools` module.
-"""
-
-import pytest
-
+from numpy.testing import assert_equal, assert_raises, assert_approx_equal
 
 from regtools import io
 
 
-class TestRegtools(object):
+rundir = os.path.abspath(os.path.dirname(__file__))
+datadir = os.path.join(rundir, 'data')
 
-    @classmethod
-    def setup_class(cls):
-        pass
 
-    def test_something(self):
-        pass
+def test_fei_metadata():
+    md = io.read_metadata_fei(os.path.join(datadir, 'sem1.tif'))
+    assert_equal(len(md), 18)
+    assert 'root' in md
+    assert 'Date' in md['root']
+    assert_equal(md['root']['Date'], '08/02/2016')
+    assert_approx_equal(float(md['Scan']['PixelHeight']), 5.39583e-9,
+                        significant=6)
 
-    @classmethod
-    def teardown_class(cls):
-        pass
 
+def test_tiff_metadata():
+    md = io.read_metadata_tiff(os.path.join(datadir, 'dstorm1.tif'))
+    assert 'x_resolution' in md
+    assert_approx_equal(md['x_resolution'], 1e-8)
